1d9ddff69d62e91dc97543cae9bc510c1b464146
Artem Suprun <efogdev@gmail.com>
2025-07-07 04:52:38 +0300

    feat: working state

--- src/behaviors/behavior_p2sm_scroll_sens.c
+++ src/behaviors/behavior_p2sm_scroll_sens.c
@@ -4,31 +4,88 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <math.h>
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
 #include <zmk/behavior.h>
+#include "drivers/p2sm_runtime.h"
+#include "dt-bindings/zmk/p2sm.h"
+#include "zmk/event_manager.h"
+#include "zmk/events/position_state_changed.h"
 
+#define CORRECTION_THRES 3 // %
 #define DT_DRV_COMPAT zmk_behavior_p2sm_scroll_sens
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+struct behavior_p2sm_scroll_sens_config {
+    uint32_t step;
+    uint32_t max_multiplier;
+};
+
+static int p2sm_detect_drift(const struct zmk_behavior_binding *binding) {
+    const struct behavior_p2sm_scroll_sens_config *cfg = zmk_behavior_get_binding(binding->behavior_dev)->config;
+    const float current = p2sm_get_yaw_coef();
+
+    if ((int16_t) (current * 1000) % cfg->step > CORRECTION_THRES * 10) {
+        const int steps_count = (int)(current * 1000 / cfg->step + 0.5f);
+        const float closest = (float)(steps_count * cfg->step) / 1000.0f;
+        LOG_WRN("Sensitivity drift detected! Setting to the closest correct value: %d%%", (int) (closest * 1000.0f));
+        p2sm_set_yaw_coef(closest);
+        return 1;
+    }
+
+    return 0;
+}
+
+static int on_p2sm_binding_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+    const struct behavior_p2sm_scroll_sens_config *cfg = zmk_behavior_get_binding(binding->behavior_dev)->config;
+    const bool direction = binding->param1 & P2SM_INC;
+    const int8_t steps = (int32_t) binding->param2;
+    const float current = p2sm_get_yaw_coef();
+    const float max_value = cfg->max_multiplier * 1.0f;
+
+    if (p2sm_detect_drift(binding)) {
+        return ZMK_BEHAVIOR_OPAQUE;
+    }
+
+    float new_val = current + (float) cfg->step * (float) steps / 1000.0f * (direction ? 1.0f : -1.0f);
+    if (new_val > max_value || new_val < 0.0f) {
+        new_val = fmod(new_val, max_value);
+        if (new_val < 0.0f) {
+            new_val = new_val + max_value;
+        }
+        LOG_WRN("Sensitivity wrapped around to stay in range!");
+    }
+
+    if (direction) {
+        LOG_INF("Sensitivity increased by %d step(s), currently at %d%%", steps, (int) (new_val * 1000.0f));
+    } else {
+        LOG_INF("Sensitivity decreased by %d step(s), currently at %d%%", steps, (int) (new_val * 1000.0f));
+    }
+
+    p2sm_set_yaw_coef(new_val);
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
-static int on_keymap_binding_released(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
-    return ZMK_BEHAVIOR_OPAQUE;
+static int behavior_p2sm_scroll_sens_init(const struct device *dev) {
+    p2sm_detect_drift(zmk_behavior_get_binding(dev->name)->config);
+    return 0;
 }
 
-static const struct behavior_driver_api behavior_none_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed,
-    .binding_released = on_keymap_binding_released,
+static const struct behavior_driver_api behavior_p2sm_scroll_sens_driver_api = {
+    .binding_pressed = on_p2sm_binding_pressed,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .get_parameter_metadata = zmk_behavior_get_empty_param_metadata,
 #endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
 };
 
-BEHAVIOR_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_none_driver_api);
+static const struct behavior_p2sm_scroll_sens_config behavior_p2sm_scroll_sens_config = {
+    .step = DT_INST_PROP(0, step),
+    .max_multiplier = DT_INST_PROP_OR(0, max_multiplier, 1),
+};
+BEHAVIOR_DT_INST_DEFINE(0, behavior_p2sm_scroll_sens_init, NULL, NULL, &behavior_p2sm_scroll_sens_config,
+    POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_p2sm_scroll_sens_driver_api);
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
--- src/behaviors/behavior_p2sm_sens.c
+++ src/behaviors/behavior_p2sm_sens.c
@@ -4,14 +4,17 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <math.h>
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
 #include <zmk/behavior.h>
 #include "drivers/p2sm_runtime.h"
 #include "dt-bindings/zmk/p2sm.h"
+#include "zmk/event_manager.h"
+#include "zmk/events/position_state_changed.h"
 
-#define CORRECTION_THRES 10
+#define CORRECTION_THRES 3 // %
 #define DT_DRV_COMPAT zmk_behavior_p2sm_sens
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
@@ -20,58 +23,66 @@ struct behavior_p2sm_sens_config {
     uint32_t step;
 };
 
-static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
-    LOG_ERR("Press");
-
+static int p2sm_detect_drift(const struct zmk_behavior_binding *binding) {
     const struct behavior_p2sm_sens_config *cfg = zmk_behavior_get_binding(binding->behavior_dev)->config;
-    const bool direction = binding->param1 | P2SM_INC;
+    const float current = p2sm_get_move_coef();
+
+    if ((int16_t) (current * 1000) % cfg->step > CORRECTION_THRES * 10) {
+        const int steps_count = (int)(current * 1000 / cfg->step + 0.5f);
+        const float closest = (float)(steps_count * cfg->step) / 1000.0f;
+        LOG_WRN("Sensitivity drift detected! Setting to the closest correct value: %d%%", (int) (closest * 1000.0f));
+        p2sm_set_move_coef(closest);
+        return 1;
+    }
+
+    return 0;
+}
+
+static int on_p2sm_binding_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+    const struct behavior_p2sm_sens_config *cfg = zmk_behavior_get_binding(binding->behavior_dev)->config;
+    const bool direction = binding->param1 & P2SM_INC;
     const int8_t steps = (int32_t) binding->param2;
     const float current = p2sm_get_move_coef();
 
-    if ((int16_t) (current * 10) % cfg->step > CORRECTION_THRES) {
-        LOG_WRN("Sensitivity value drift detected! Correction applied.");
-        p2sm_set_move_coef(1.0f);
+    if (p2sm_detect_drift(binding)) {
         return ZMK_BEHAVIOR_OPAQUE;
     }
 
-    float new_val = current + (float) cfg->step * (float) steps / 10.0f * (direction ? 1.0f : -1.0f);
-    if (new_val > 1.0f) {
-        new_val = new_val - 1.0f;
-    }
-    if (new_val < 0.0f) {
-        new_val = new_val + 1.0f;
+    float new_val = current + (float) cfg->step * (float) steps / 1000.0f * (direction ? 1.0f : -1.0f);
+    if (new_val > 1.0f || new_val < 0.0f) {
+        new_val = new_val - floor(new_val);
+        if (new_val < 0.0f) {
+            new_val = new_val + 1.0f;
+        }
+        LOG_WRN("Sensitivity wrapped around to stay in range!");
     }
 
     if (direction) {
-        LOG_INF("Sensitivity increased by %d step(s)", steps);
+        LOG_INF("Sensitivity increased by %d step(s), currently at %d%%", steps, (int) (new_val * 1000.0f));
     } else {
-        LOG_INF("Sensitivity decreased by %d step(s)", steps);
+        LOG_INF("Sensitivity decreased by %d step(s), currently at %d%%", steps, (int) (new_val * 1000.0f));
     }
 
     p2sm_set_move_coef(new_val);
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
-static int on_keymap_binding_released(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
-    LOG_ERR("Release");
-    return ZMK_BEHAVIOR_OPAQUE;
+static int behavior_p2sm_sens_init(const struct device *dev) {
+    p2sm_detect_drift(zmk_behavior_get_binding(dev->name)->config);
+    return 0;
 }
 
-static const struct behavior_driver_api api = {
-    .binding_pressed = on_keymap_binding_pressed,
-    .binding_released = on_keymap_binding_released,
+static const struct behavior_driver_api behavior_p2sm_sens_driver_api = {
+    .binding_pressed = on_p2sm_binding_pressed,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .get_parameter_metadata = zmk_behavior_get_empty_param_metadata,
 #endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
 };
 
-#define RST_INST(n)                                                          \
-    static const struct behavior_p2sm_sens_config p2sm_config_##n = {        \
-        .step = DT_INST_PROP(0, step),                                       \
-    };                                                                       \
-    BEHAVIOR_DT_INST_DEFINE(0, NULL, NULL, NULL, &p2sm_config_##n,           \
-        POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &api);
-
-DT_INST_FOREACH_STATUS_OKAY(RST_INST)
+static const struct behavior_p2sm_sens_config behavior_p2sm_sens_config = {
+    .step = DT_INST_PROP(0, step),
+};
+BEHAVIOR_DT_INST_DEFINE(0, behavior_p2sm_sens_init, NULL, NULL, &behavior_p2sm_sens_config,
+    POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_p2sm_sens_driver_api);
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
